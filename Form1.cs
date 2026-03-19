42647190587337cafe623261cd8b078865b614f3
Mario Gallardo <macoktt@gmail.com>
2026-03-18 23:11:53 -0500

    Add files via upload

--- Form1.cs
+++ Form1.cs
@@ -4,11 +4,12 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Practica__3
+namespace Practica___2
 {
     public partial class Form1 : Form
     {
@@ -17,29 +18,15 @@ namespace Practica__3
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            int num1 = Int32.Parse(textBox1.Text);
-            int num2 = Int32.Parse(textBox2.Text);
-
-            int sum = num1 + num2;
-
-            label3.Text = "Sum=" + sum;
+            label3.Text = "Hello," + textBox1.Text + "!";
         }
-
-        private void label3_Click(object sender, EventArgs e)
+   
+private void label3_Click(object sender, EventArgs e)
         {
 
         }
     }
+
 }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Practica__3
+namespace Practica___2
 {
     internal static class Program
     {
